b105bf6e78002780f802a6a23951dbc06b697571
deltoss <mtran0011@outlook.com>
2025-05-28 11:42:43 +1000

    Clarified description of LazyGit for file

--- lua/custom/plugins/lazygit-nvim.lua
+++ lua/custom/plugins/lazygit-nvim.lua
@@ -16,6 +16,6 @@ return {
   -- order to load the plugin when the command is run for the first time
   keys = {
     { '<leader>gg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
-    { '<leader>gf', '<cmd>LazyGitCurrentFile<cr>', desc = 'LazyGit' },
+    { '<leader>gf', '<cmd>LazyGitCurrentFile<cr>', desc = 'LazyGit for current file' },
   },
 }
