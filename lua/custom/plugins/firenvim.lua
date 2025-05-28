c84a988d3c4c1f0a9fea6c92638dbc4b4d56018e
deltoss <mtran0011@outlook.com>
2025-05-29 09:08:51 +1000

    Added specific keymaps for firenvim

--- lua/custom/plugins/firenvim.lua
+++ lua/custom/plugins/firenvim.lua
@@ -1,3 +1,13 @@
+local keymap = vim.keymap.set
+
+if vim.g.started_by_firenvim then
+  keymap('n', 'q', ':wq<CR>', { noremap = true, silent = true })
+  vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+    pattern = 'github.com_*.txt',
+    command = 'set filetype=markdown',
+  })
+end
+
 return {
   'glacambre/firenvim',
   build = ':call firenvim#install(0)',
