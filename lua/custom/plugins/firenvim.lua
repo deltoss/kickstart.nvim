4d31bc5714e07df25a3c29da0e2414797e1981e6
EmTee <mtran0011@outlook.com>
2025-06-01 08:39:27 +1000

    Added keymap for enter key to save

--- lua/custom/plugins/firenvim.lua
+++ lua/custom/plugins/firenvim.lua
@@ -2,6 +2,7 @@ local keymap = vim.keymap.set
 
 if vim.g.started_by_firenvim then
   keymap('n', 'q', ':wq<CR>', { noremap = true, silent = true })
+  keymap('n', '<CR>', ':wq<CR>', { noremap = true, silent = true })
   vim.api.nvim_create_autocmd({ 'BufEnter' }, {
     pattern = 'github.com_*.txt',
     command = 'set filetype=markdown',
