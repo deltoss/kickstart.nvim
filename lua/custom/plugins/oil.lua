cba1e26fa8c015ccfd28e862e2688466e68d95dd
deltoss <mtran0011@outlook.com>
2025-06-03 13:53:01 +1000

    Adjusted Oil.nvim keymaps to follow main keymaps concepts

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -8,6 +8,17 @@ return {
     view_options = {
       show_hidden = true,
     },
+    keymaps = {
+      ['<C-s>'] = false,
+      ['<C-h>'] = false,
+      ['<leader><Right>'] = { 'actions.select', opts = { vertical = true } },
+      ['<leader><Left>'] = { 'actions.select', opts = { vertical = true, split = 'aboveleft' } },
+      ['<leader><Up>'] = { 'actions.select', opts = { horizontal = true, split = 'aboveleft' } },
+      ['<leader><Down>'] = { 'actions.select', opts = { horizontal = true } },
+      ['<leader>t'] = { 'actions.select', opts = { tab = true } },
+      ['<leader>p'] = 'actions.preview',
+      ['<leader>c'] = { 'actions.close', mode = 'n' },
+    },
   },
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   keys = {
@@ -16,4 +27,3 @@ return {
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
 }
-
