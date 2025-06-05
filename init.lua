458d5a609867bd91aebf9d61acdf0e5da72243f6
deltoss <mtran0011@outlook.com>
2025-06-05 14:14:40 +1000

    Added custom snippets and enabled premade snippets

--- init.lua
+++ init.lua
@@ -808,12 +808,15 @@ require('lazy').setup({
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+
+              -- See Adding Snippets: https://github.com/L3MON4D3/LuaSnip/blob/master/DOC.md#lua
+              require('luasnip.loaders.from_lua').load { paths = './lua/custom/snippets' }
+            end,
+          },
         },
         opts = {},
       },
