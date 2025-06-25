4e4b3823890b967bf6247c018d56d4e920cae987
EmTee <mtran0011@outlook.com>
2025-06-25 10:15:43 +1000

    Added telescope-everything.nvim plugin

---
+++ lua/custom/plugins/telescope-everything-nvim.lua
@@ -0,0 +1,10 @@
+return {
+  'deltoss/telescope-everything.nvim',
+  dependencies = { 'nvim-telescope/telescope.nvim' },
+  config = function()
+    require('telescope').load_extension 'everything'
+  end,
+  keys = {
+    { '<leader>se', ':Telescope everything<CR>', desc = '[S]earch [E]verything' },
+  },
+}
