5daf2af53b99d4c5370c7abf37872ece7f359e2a
EmTee <mtran0011@outlook.com>
2025-08-09 20:39:03 +1000

    Added tmpl and templ syntax highlighting

---
+++ lua/custom/plugins/conform-nvim.lua
@@ -0,0 +1,26 @@
+return {
+  'stevearc/conform.nvim',
+  opts = {
+    format_on_save = {
+      -- These options will be passed to conform.format()
+      timeout_ms = 500,
+      lsp_format = 'fallback',
+    },
+    formatters_by_ft = {
+      csharp = { 'csharpier' },
+      css = { 'css_beautify' },
+      json = { 'jq' },
+      yaml = { 'yq' },
+      xml = { 'yq' },
+      toml = { 'taplo' },
+      templ = { 'templ' },
+      lua = { 'stylua' },
+      -- Conform will run multiple formatters sequentially
+      python = { 'isort', 'black' },
+      -- You can customize some of the format options for the filetype (:help conform.format)
+      rust = { 'rustfmt', lsp_format = 'fallback' },
+      -- Conform will run the first available formatter
+      javascript = { 'prettierd', 'prettier', stop_after_first = true },
+    },
+  },
+}
