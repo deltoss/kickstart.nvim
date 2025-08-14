f59d35d0547aee6aab7d845478c2a3fb35734af5
Michael Tran <mtran0011@outlook.com>
2025-08-15 07:38:23 +1000

    Configured project.nvim to be lazy loaded

--- lua/custom/plugins/project-nvim.lua
+++ lua/custom/plugins/project-nvim.lua
@@ -1,6 +1,6 @@
 return {
   'ahmedkhalf/project.nvim',
-  lazy = false,
+  event = 'VeryLazy',
   config = function()
     require('project_nvim').setup {
       patterns = { '.git', 'Makefile', '.svn', '*.sln', 'package.json', 'readme.md' },
