3b11fe9ec1f252865223d9be5dc29ed6b9c4d068
m.tran <m.tran@leapdev.io>
2025-09-30 10:40:49 +1000

    Added diffview, and readjusted keymaps accordingly

---
+++ lua/custom/plugins/diffview-nvim.lua
@@ -0,0 +1,37 @@
+local function toggle_diffview(cmd)
+  if next(require('diffview.lib').views) == nil then
+    vim.cmd(cmd)
+  else
+    vim.cmd 'DiffviewClose'
+  end
+end
+
+return {
+  'sindrets/diffview.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  cmd = { 'DiffviewOpen', 'DiffviewClose', 'DiffviewToggleFiles', 'DiffviewFocusFiles', 'DiffviewRefresh' },
+  opts = {},
+  keys = {
+    {
+      '<leader>gd',
+      function()
+        toggle_diffview 'DiffviewOpen'
+      end,
+      desc = '[G]it [D]iff Index',
+    },
+    {
+      '<leader>gD',
+      function()
+        toggle_diffview 'DiffviewOpen master..HEAD'
+      end,
+      desc = '[G]it [D]iff master',
+    },
+    {
+      '<leader>gh',
+      function()
+        toggle_diffview 'DiffviewFileHistory %'
+      end,
+      desc = 'Open [G]it [H]istory for current File',
+    },
+  },
+}
