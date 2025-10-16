f604e83262b26ecee1803a71b069e6eabfba7b68
EmTee <mtran0011@outlook.com>
2025-10-16 15:12:19 +1100

    Removed oil.nvim in favor of yazi.nvim

---
+++ lua/custom/plugins/yazi-nvim.lua
@@ -0,0 +1,52 @@
+---@type LazySpec
+return {
+  'mikavilpas/yazi.nvim',
+  version = '*', -- use the latest stable version
+  event = 'VeryLazy',
+  dependencies = {
+    { 'nvim-lua/plenary.nvim', lazy = true },
+  },
+  keys = {
+    {
+      '-',
+      mode = { 'n', 'v' },
+      '<cmd>Yazi<cr>',
+      desc = 'Open yazi at the current file',
+    },
+    {
+      -- Open in the current working directory
+      '<leader>_',
+      '<cmd>Yazi cwd<cr>',
+      desc = "Open the file manager in nvim's working directory",
+    },
+    {
+      '<leader>-',
+      '<cmd>Yazi toggle<cr>',
+      desc = 'Resume the last yazi session',
+    },
+  },
+  ---@type YaziConfig | {}
+  opts = {
+    open_for_directories = true,
+    change_neovim_cwd_on_close = true,
+    keymaps = {
+      show_help = '<f2>',
+      open_file_in_vertical_split = '<c-Right>',
+      open_file_in_horizontal_split = '<c-Down>',
+      open_file_in_tab = '<c-t>',
+      grep_in_directory = '<c-s>',
+      replace_in_directory = '<c-g>',
+      cycle_open_buffers = '<tab>',
+      copy_relative_path_to_selected_files = '<c-y>',
+      send_to_quickfix_list = '<c-q>',
+      change_working_directory = '~',
+      open_and_pick_window = '<c-o>',
+    },
+  },
+  init = function()
+    -- mark netrw as loaded so it's not loaded at all.
+    --
+    -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+    vim.g.loaded_netrwPlugin = 1
+  end,
+}
