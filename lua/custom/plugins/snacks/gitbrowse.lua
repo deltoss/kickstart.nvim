c21d8c918ceb94d4ddb13df9d155294d79b4e9eb
m.tran <m.tran@leapdev.io>
2025-11-19 15:26:19 +1100

    Updated bitbucket to use the correct link format

--- lua/custom/plugins/snacks/gitbrowse.lua
+++ lua/custom/plugins/snacks/gitbrowse.lua
@@ -2,7 +2,17 @@ return {
   'snacks.nvim', -- only the name of the plugin needs to be specified since it already exists
   ---@type snacks.Config
   opts = {
-    gitbrowse = { enabled = true },
+    gitbrowse = {
+      enabled = true,
+      url_patterns = {
+        ['bitbucket%.org'] = {
+          branch = '/src/{branch}',
+          file = '/src/{branch}/{file}#lines-{line_start}:{line_end}',
+          permalink = '/src/{commit}/{file}#lines-{line_start}:{line_end}',
+          commit = '/commits/{commit}',
+        },
+      },
+    },
   },
   keys = {
     {
