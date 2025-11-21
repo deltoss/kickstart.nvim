8c5aaf456133699cd2499d8260339caecdd2f3c1
m.tran <m.tran@leapdev.io>
2025-11-21 16:29:46 +1100

    Improved the autocmd script to adjust cwd dynamically

--- lua/custom/autocmds.lua
+++ lua/custom/autocmds.lua
@@ -4,10 +4,12 @@ vim.api.nvim_create_autocmd('BufReadPost', {
     local file_dir = vim.fn.expand '%:p:h'
 
     local cmd = string.format('cd %s && git rev-parse --show-toplevel', vim.fn.shellescape(file_dir))
-    local root = vim.fn.system(cmd):gsub('\n', '')
+    local git_root = vim.fn.system(cmd):gsub('\n', '')
 
     if vim.v.shell_error == 0 then
-      vim.cmd('cd ' .. root)
+      vim.cmd('cd ' .. vim.fn.fnameescape(git_root))
+    else
+      vim.cmd('cd ' .. vim.fn.fnameescape(file_dir))
     end
   end,
 })
