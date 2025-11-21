b9af22aa9d304035f25f7a31d5c7ad32a111a301
m.tran <m.tran@leapdev.io>
2025-11-21 17:08:31 +1100

    Adjusted notifier to move out of the way

--- lua/custom/plugins/snacks/notifier.lua
+++ lua/custom/plugins/snacks/notifier.lua
@@ -2,7 +2,13 @@ return {
   'snacks.nvim', -- only the name of the plugin needs to be specified since it already exists
   ---@type snacks.Config
   opts = {
-    notifier = { enabled = true },
+    notifier = {
+      enabled = true,
+      timeout = 8000,
+      ---@type snacks.notifier.style
+      style = 'fancy',
+      top_down = false, -- place notifications from bottom to top
+    },
   },
   keys = {
     {
