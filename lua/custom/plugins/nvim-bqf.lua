ab7ac4a13bbaed0a5f23cee17dad08857fb4b368
m.tran <m.tran@leapdev.io>
2025-11-21 11:31:23 +1100

    Removed nvim bqf in favor of the newer, better and shiny quicker nvim

--- lua/custom/plugins/nvim-bqf.lua
+++
@@ -1,5 +0,0 @@
-return {
-  -- Improves the in-built quickfix feature with previews
-  'kevinhwang91/nvim-bqf',
-  ft = 'qf',
-}
---
+++ lua/custom/plugins/quicker-nvim.lua
@@ -0,0 +1,37 @@
+vim.keymap.set('n', '<leader>q', function()
+  require('quicker').toggle()
+end, {
+  desc = 'Toggle [Q]uickfix',
+})
+
+vim.keymap.set('n', '<leader>l', function()
+  require('quicker').toggle { loclist = true }
+end, {
+  desc = 'Toggle [L]oclist', -- Basically, quickfix for local window only
+})
+
+-- Quickfix: Use Snacks picker or telescope search, then load it into quickfix for navigation with <C-q>
+return {
+  'stevearc/quicker.nvim',
+  ft = 'qf',
+  ---@module "quicker"
+  ---@type quicker.SetupOptions
+  opts = {
+    keys = {
+      {
+        '>',
+        function()
+          require('quicker').expand { before = 2, after = 2, add_to_existing = true }
+        end,
+        desc = 'Expand quickfix context',
+      },
+      {
+        '<',
+        function()
+          require('quicker').collapse()
+        end,
+        desc = 'Collapse quickfix context',
+      },
+    },
+  },
+}
