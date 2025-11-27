28aefad43e5f7b949797f514e83cbd96381c5d9c
m.tran <m.tran@leapdev.io>
2025-11-27 15:04:13 +1100

    Adjusted bigfile conditions

--- lua/custom/plugins/snacks/bigfile.lua
+++ lua/custom/plugins/snacks/bigfile.lua
@@ -2,6 +2,11 @@ return {
   'snacks.nvim', -- only the name of the plugin needs to be specified since it already exists
   ---@type snacks.Config
   opts = {
-    bigfile = { enabled = true },
+    bigfile = {
+      enabled = true,
+      -- Average line length (useful for minified files)
+      -- increased from default to handle large json, xml, yaml or toml files
+      line_length = 250000,
+    },
   },
 }
