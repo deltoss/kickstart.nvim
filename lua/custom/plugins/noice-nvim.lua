c6f081632a579f255445cf75bbc2ee93421f2685
m.tran <m.tran@leapdev.io>
2025-12-09 10:23:00 +1100

    Disabled Noice progress as it have issues with roslyn.nvim

--- lua/custom/plugins/noice-nvim.lua
+++ lua/custom/plugins/noice-nvim.lua
@@ -8,6 +8,9 @@ return {
         ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
         ['vim.lsp.util.stylize_markdown'] = true,
       },
+      progress = {
+        enabled = false, -- Turn this off as it breaks roslyn compiler. See https://github.com/seblyng/roslyn.nvim/issues/236
+      },
     },
     -- you can enable a preset for easier configuration
     presets = {
