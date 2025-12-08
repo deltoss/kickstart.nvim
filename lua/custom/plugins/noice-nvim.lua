be78f1643475fe36b5a61f26cc9d0c7b0a26cb86
m.tran <m.tran@leapdev.io>
2025-12-09 09:17:52 +1100

    Added recommended settings for noice.nvim

--- lua/custom/plugins/noice-nvim.lua
+++ lua/custom/plugins/noice-nvim.lua
@@ -1,7 +1,19 @@
 return {
   'folke/noice.nvim',
   event = 'VeryLazy',
-  opts = {},
+  opts = {
+    lsp = {
+      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+      override = {
+        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+        ['vim.lsp.util.stylize_markdown'] = true,
+      },
+    },
+    -- you can enable a preset for easier configuration
+    presets = {
+      long_message_to_split = true, -- long messages will be sent to a split
+    },
+  },
   dependencies = {
     'MunifTanjim/nui.nvim',
   },
