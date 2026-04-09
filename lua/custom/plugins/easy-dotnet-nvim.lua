511770bd18b1da75a1391b0c6d679877e0754ca1
deltoss <m.tran@leapdev.io>
2026-04-09 16:03:11 +1000

    Added more keymaps for easy-dotnet

--- lua/custom/plugins/easy-dotnet-nvim.lua
+++ lua/custom/plugins/easy-dotnet-nvim.lua
@@ -30,11 +30,32 @@ return {
   end,
   keys = {
     { '<localleader><localleader>', '<cmd>Dotnet<cr>', desc = 'List Dotnet Commands', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
+
     { '<localleader>b', '<cmd>Dotnet build quickfix<cr>', desc = '[B]uild', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
     { '<localleader>B', '<cmd>Dotnet build solution quickfix<cr>', desc = '[B]uild Solution', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
+    { '<localleader>c', '<cmd>Dotnet clean<cr>', desc = '[C]lean', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
     { '<localleader>r', '<cmd>Dotnet clean<cr><cmd>Dotnet build quickfix<cr>', desc = '[R]ebuild', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
     { '<localleader>R', '<cmd>Dotnet restore<cr>', desc = '[R]estore', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
+
+    { '<localleader>p', '<cmd>Dotnet project view<cr>', desc = '[P]roject View', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
+    { '<localleader>P', '<cmd>Dotnet project view<cr>', desc = '[P]roject View for Default Project', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
+
     { '<localleader>D', '<cmd>Dotnet run profile<cr>', desc = 'Run without [D]ebugging', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
     { '<localleader>d', '<cmd>Dotnet debug profile<cr>', desc = '[D]ebug', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
+    { '<localleader>o', '<cmd>Dotnet terminal toggle<cr>', desc = '[O]utput', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
+
+    { '<localleader>n', '<cmd>Dotnet createfile<cr>', desc = '[N]ew File', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
+    { '<localleader>N', '<cmd>Dotnet new<cr>', desc = '[N]ew Project', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
+
+    { '<localleader>S', '<cmd>Dotnet secrets<cr>', desc = 'User [S]ecrets', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
+
+    { '<localleader>ii', '<cmd>Dotnet diagnostic<cr>', desc = '[I]nformation', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
+    { '<localleader>ie', '<cmd>Dotnet diagnostic errors<cr>', desc = '[I]nformation [E]rrors', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
+    {
+      '<localleader>iw',
+      '<cmd>Dotnet diagnostic warnings<cr>',
+      desc = '[I]nformation [W]arnings',
+      ft = { 'cs', 'solution', 'slnfilter', 'csproj' },
+    },
   },
 }
