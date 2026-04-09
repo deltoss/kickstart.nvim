0a4f239f3d577d5712ddc5f8456503ed04120718
deltoss <m.tran@leapdev.io>
2026-04-09 11:53:11 +1000

    Added support for lazyloaded keymaps for easy-dotnet-nvim.lua

--- lua/custom/plugins/easy-dotnet-nvim.lua
+++ lua/custom/plugins/easy-dotnet-nvim.lua
@@ -18,13 +18,23 @@ return {
       log_level = 'Verbose',
     },
   },
+  init = function()
+    vim.filetype.add {
+      extension = {
+        sln = 'solution',
+        slnx = 'solution',
+        slnf = 'slnfilter',
+        csproj = 'csproj',
+      },
+    }
+  end,
   keys = {
-    { '<leader><leader>nn', '<cmd>Dotnet<cr>', desc = 'List Dot[N]et Commands' },
-    { '<leader><leader>nbb', '<cmd>Dotnet build quickfix<cr>', desc = '[B]uild' },
-    { '<leader><leader>nbB', '<cmd>Dotnet build solution quickfix<cr>', desc = '[B]uild Solution' },
-    { '<leader><leader>nbr', '<cmd>Dotnet clean<cr><cmd>Dotnet build quickfix<cr>', desc = '[R]ebuild' },
-    { '<leader><leader>nbR', '<cmd>Dotnet restore<cr>', desc = '[R]estore' },
-    { '<leader><leader>nrr', '<cmd>Dotnet run profile<cr>', desc = '[R]un' },
-    { '<leader><leader>nrd', '<cmd>Dotnet debug profile<cr>', desc = '[D]ebug' },
+    { '<localleader><localleader>', '<cmd>Dotnet<cr>', desc = 'List Dotnet Commands', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
+    { '<localleader>b', '<cmd>Dotnet build quickfix<cr>', desc = '[B]uild', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
+    { '<localleader>B', '<cmd>Dotnet build solution quickfix<cr>', desc = '[B]uild Solution', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
+    { '<localleader>r', '<cmd>Dotnet clean<cr><cmd>Dotnet build quickfix<cr>', desc = '[R]ebuild', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
+    { '<localleader>R', '<cmd>Dotnet restore<cr>', desc = '[R]estore', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
+    { '<localleader>D', '<cmd>Dotnet run profile<cr>', desc = 'Run without [D]ebugging', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
+    { '<localleader>d', '<cmd>Dotnet debug profile<cr>', desc = '[D]ebug', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
   },
 }
