8f8f71cf48d0fd03809d892de63e980e91169352
deltoss <m.tran@leapdev.io>
2026-04-09 17:02:56 +1000

    Added additional keymaps for easy-dotnet

--- lua/custom/plugins/easy-dotnet-nvim.lua
+++ lua/custom/plugins/easy-dotnet-nvim.lua
@@ -1,3 +1,17 @@
+local function pick_sln_file(callback)
+  Snacks.picker.files {
+    prompt = 'Select solution file',
+    cmd = 'fd',
+    args = { '--extension', 'sln', '--extension', 'slnx' },
+    confirm = function(picker, item)
+      picker:close()
+      if item then
+        callback(item.file)
+      end
+    end,
+  }
+end
+
 return {
   'GustavEikaas/easy-dotnet.nvim',
   dependencies = { 'nvim-lua/plenary.nvim', 'mfussenegger/nvim-dap', 'folke/snacks.nvim' },
@@ -38,15 +52,32 @@ return {
     { '<localleader>R', '<cmd>Dotnet restore<cr>', desc = '[R]estore', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
 
     { '<localleader>p', '<cmd>Dotnet project view<cr>', desc = '[P]roject View', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
-    { '<localleader>P', '<cmd>Dotnet project view<cr>', desc = '[P]roject View for Default Project', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
+    { '<localleader>Pa', '<cmd>Dotnet add package<cr>', desc = '[A]dd', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
+    { '<localleader>PA', '<cmd>Dotnet add package prerelease<cr>', desc = '[A]dd Prerelease', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
+    { '<localleader>Pd', '<cmd>Dotnet add remove<cr>', desc = '[D]elete', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
 
     { '<localleader>D', '<cmd>Dotnet run profile<cr>', desc = 'Run without [D]ebugging', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
     { '<localleader>d', '<cmd>Dotnet debug profile<cr>', desc = '[D]ebug', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
     { '<localleader>o', '<cmd>Dotnet terminal toggle<cr>', desc = '[O]utput', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
+    { '<localleader>O', '<cmd>Dotnet outdated<cr>', desc = '[O]utdated', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
 
     { '<localleader>n', '<cmd>Dotnet createfile<cr>', desc = '[N]ew File', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
     { '<localleader>N', '<cmd>Dotnet new<cr>', desc = '[N]ew Project', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
 
+    { '<localleader>w', '<cmd>Dotnet watch<cr>', desc = '[W]atch', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
+
+    {
+      '<localleader>ss',
+      function()
+        pick_sln_file(function(path)
+          require('easy-dotnet').solution_select(path)
+        end)
+      end,
+      desc = '[A]dd',
+      ft = { 'cs', 'solution', 'slnfilter', 'csproj' },
+    },
+    { '<localleader>sa', '<cmd>Dotnet solution add<cr>', desc = '[A]dd', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
+    { '<localleader>sd', '<cmd>Dotnet solution remove<cr>', desc = '[D]elete/Remove', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
     { '<localleader>S', '<cmd>Dotnet secrets<cr>', desc = 'User [S]ecrets', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
 
     { '<localleader>ii', '<cmd>Dotnet diagnostic<cr>', desc = '[I]nformation', ft = { 'cs', 'solution', 'slnfilter', 'csproj' } },
