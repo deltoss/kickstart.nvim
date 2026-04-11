45000055841f8e2ded1a7ca51f7be0875858bab0
emtee <mtran0011@outlook.com>
2026-04-11 15:52:01 +1000

    Fixed grep shortcut, and set A keymap to create easy-dotnet items

--- lua/custom/plugins/yazi-nvim.lua
+++ lua/custom/plugins/yazi-nvim.lua
@@ -34,7 +34,7 @@ return {
       open_file_in_vertical_split = '<c-Right>',
       open_file_in_horizontal_split = '<c-Down>',
       open_file_in_tab = '<c-t>',
-      grep_in_directory = '<c-s>',
+      grep_in_directory = '<leader>sg',
       replace_in_directory = '<c-g>',
       cycle_open_buffers = '<tab>',
       copy_relative_path_to_selected_files = '<c-y>',
@@ -42,6 +42,22 @@ return {
       change_working_directory = '~',
       open_and_pick_window = '<c-o>',
     },
+    -- completely override the keymappings for yazi. This function will be
+    -- called in the context of the yazi terminal buffer.
+    set_keymappings_function = function(buffer_id, config, context)
+      vim.keymap.set({ 't' }, 'A', function()
+        local entry = context.ya_process.hovered_url
+        if entry == nil then
+          vim.notify('No fd entry in yazi.nvim', vim.log.levels.WARN)
+          return
+        end
+        local stat = vim.uv.fs_stat(entry)
+        local path = stat.type == 'directory' and entry or vim.fs.dirname(entry)
+        require('easy-dotnet').create_new_item(path, function(item_path)
+          vim.cmd.edit(item_path)
+        end)
+      end, { buffer = buffer_id })
+    end,
   },
   init = function()
     -- mark netrw as loaded so it's not loaded at all.
