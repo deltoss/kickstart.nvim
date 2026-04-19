80964a4aff5c479d3b9413c3d56fe4e149d06667
emtee <mtran0011@outlook.com>
2026-04-19 19:14:09 +1000

    Enabled core ui that comes with neovim 0.12

--- init.lua
+++ init.lua
@@ -90,6 +90,8 @@ P.S. You can delete this when you're done too. It's your config now! :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = '_'
 
+require('vim._core.ui2').enable {}
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
