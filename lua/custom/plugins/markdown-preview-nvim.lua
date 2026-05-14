8dfa1d23207b82b20cc6fc288b39b696e6148d6d
deltoss <m.tran@leapdev.io>
2026-05-14 10:02:17 +1000

    Fixed bad installation with markdown preview

--- lua/custom/plugins/markdown-preview-nvim.lua
+++ lua/custom/plugins/markdown-preview-nvim.lua
@@ -1,21 +1,9 @@
 return {
-  -- Install markdown preview, use npx if available.
-  'iamcco/markdown-preview.nvim',
-  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-  ft = { 'markdown' },
-  build = function(plugin)
-    if vim.fn.executable 'npx' then
-      vim.cmd('!cd ' .. plugin.dir .. ' && cd app && npx --yes yarn install')
-    else
-      vim.cmd [[Lazy load markdown-preview.nvim]]
-      vim.fn['mkdp#util#install']()
-    end
-  end,
-  init = function()
-    if vim.fn.executable 'npx' then
-      vim.g.mkdp_filetypes = { 'markdown' }
-    end
-  end,
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  ft = { "markdown" },
+  build = function() vim.fn["mkdp#util#install"]() end,
+  ft = { "markdown" },
   keys = {
     { '<leader>Mp', '<cmd>MarkdownPreviewToggle<cr>', desc = '[P]review Toggle' },
   },
