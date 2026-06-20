77a5608644cccb86ee2ebb385f03d4180169c989
emtee <mtran0011@outlook.com>
2026-06-21 08:14:52 +1000

    Fixed zellij integration

--- lua/custom/plugins/smart-splits-nvim.lua
+++ lua/custom/plugins/smart-splits-nvim.lua
@@ -1,7 +1,36 @@
--- There are issues with Zellij integration, so disable it completely
-vim.g.smart_splits_multiplexer_integration = false
 return {
   'mrjones2014/smart-splits.nvim',
+  opts = {
+    -- Desired behavior when your cursor is at an edge and you
+    -- are moving towards that same edge:
+    -- 'wrap' => Wrap to opposite side
+    -- 'split' => Create a new split in the desired direction
+    -- 'stop' => Do nothing
+    -- function => You handle the behavior yourself
+    -- NOTE: If using a function, the function will be called with
+    -- a context object with the following fields:
+    -- {
+    --    mux = {
+    --      type:'tmux'|'wezterm'|'kitty'|'zellij'
+    --      current_pane_id():number,
+    --      is_in_session(): boolean
+    --      current_pane_is_zoomed():boolean,
+    --      -- following methods return a boolean to indicate success or failure
+    --      current_pane_at_edge(direction:'left'|'right'|'up'|'down'):boolean
+    --      next_pane(direction:'left'|'right'|'up'|'down'):boolean
+    --      resize_pane(direction:'left'|'right'|'up'|'down'):boolean
+    --      split_pane(direction:'left'|'right'|'up'|'down',size:number|nil):boolean
+    --    },
+    --    direction = 'left'|'right'|'up'|'down',
+    --    split(), -- utility function to split current Neovim pane in the current direction
+    --    wrap(), -- utility function to wrap to opposite Neovim pane
+    -- }
+    -- NOTE: `at_edge = 'wrap'` is not supported on Kitty terminal
+    -- multiplexer, as there is no way to determine layout via the CLI
+    at_edge = function(context)
+      vim.cmd 'silent !zellij action switch-mode normal'
+    end,
+  },
   keys = {
     {
       '<S-Left>',
