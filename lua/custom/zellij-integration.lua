bd36ca351cbd2de8774d7772a4c86d96ef8aed3e
deltoss <m.tran@leapdev.io>
2026-08-05 10:08:20 +1000

    Improved to use OSC codes where possible

--- lua/custom/zellij-integration.lua
+++ lua/custom/zellij-integration.lua
@@ -21,28 +21,30 @@ keymap('n', '<leader>w<Left>', function()
 end, { desc = 'Split Pane Left' })
 
 if vim.env.ZELLIJ then
-  local last_name = nil
+  -- Pane title via OSC 2. A CLI-set pane name overrides it, so clear any.
+  vim.fn.jobstart({ 'zellij', 'action', 'undo-rename-pane' }, { detach = true })
+  vim.o.title = true
+  vim.o.titlestring = [[👾 %{bufname() == '' ? 'nvim' : 'nvim ' .. expand('%:p:~')}]]
 
-  local function rename_pane()
-    local buf = vim.api.nvim_get_current_buf()
-    if vim.bo[buf].buftype ~= '' then
-      return
-    end
+  local last_tail = nil
 
-    local file = vim.api.nvim_buf_get_name(buf)
-    local name = file == '' and 'nvim' or ('nvim ' .. vim.fn.fnamemodify(file, ':p:~'))
-    local tail = file == '' and 'nvim' or vim.fn.fnamemodify(file, ':t')
-    if name == last_name then
-      return
-    end
-    last_name = name
-
-    vim.fn.jobstart({ 'zellij', 'action', 'rename-pane', '👾 ' .. name }, { detach = true })
-    vim.fn.jobstart({ 'zellij', 'action', 'rename-tab', '👾 ' .. tail }, { detach = true })
-  end
-
+  -- Tab rename has no OSC equivalent, needs the CLI
   vim.api.nvim_create_autocmd({ 'BufEnter', 'BufFilePost' }, {
-    group = vim.api.nvim_create_augroup('ZellijPaneName', { clear = true }),
-    callback = rename_pane,
+    group = vim.api.nvim_create_augroup('ZellijTabName', { clear = true }),
+    callback = function()
+      local buf = vim.api.nvim_get_current_buf()
+      if vim.bo[buf].buftype ~= '' then
+        return
+      end
+
+      local file = vim.api.nvim_buf_get_name(buf)
+      local tail = file == '' and 'nvim' or vim.fn.fnamemodify(file, ':t')
+      if tail == last_tail then
+        return
+      end
+      last_tail = tail
+
+      vim.fn.jobstart({ 'zellij', 'action', 'rename-tab', '👾 ' .. tail }, { detach = true })
+    end,
   })
 end
