96510c88d62917dd5790798139ce0f37883d48d3
emtee <mtran0011@outlook.com>
2026-08-08 10:04:00 +1000

    Fixed new window not inheriting the CWD of neovim

--- lua/custom/zellij-integration.lua
+++ lua/custom/zellij-integration.lua
@@ -1,23 +1,27 @@
 local keymap = vim.keymap.set
 
+-- A trailing command is required for zellij to apply --cwd to new-pane,
+-- so reuse Neovim's own shell (see use-nushell.lua) as that command.
+local shell = vim.o.shell
+
 keymap('n', '<leader>w<Up>', function()
   local cwd = vim.fn.getcwd()
-  vim.fn.jobstart({ 'zellij', 'action', 'new-pane', '--direction', 'up', '--cwd', cwd }, { detach = true })
+  vim.fn.jobstart({ 'zellij', 'action', 'new-pane', '--direction', 'up', '--cwd', cwd, '--close-on-exit', '--', shell }, { detach = true })
 end, { desc = 'Split Pane Top' })
 
 keymap('n', '<leader>w<Right>', function()
   local cwd = vim.fn.getcwd()
-  vim.fn.jobstart({ 'zellij', 'action', 'new-pane', '--direction', 'right', '--cwd', cwd }, { detach = true })
+  vim.fn.jobstart({ 'zellij', 'action', 'new-pane', '--direction', 'right', '--cwd', cwd, '--close-on-exit', '--', shell }, { detach = true })
 end, { desc = 'Split Pane Right' })
 
 keymap('n', '<leader>w<Down>', function()
   local cwd = vim.fn.getcwd()
-  vim.fn.jobstart({ 'zellij', 'action', 'new-pane', '--direction', 'down', '--cwd', cwd }, { detach = true })
+  vim.fn.jobstart({ 'zellij', 'action', 'new-pane', '--direction', 'down', '--cwd', cwd, '--close-on-exit', '--', shell }, { detach = true })
 end, { desc = 'Split Pane Down' })
 
 keymap('n', '<leader>w<Left>', function()
   local cwd = vim.fn.getcwd()
-  vim.fn.jobstart({ 'zellij', 'action', 'new-pane', '--direction', 'left', '--cwd', cwd }, { detach = true })
+  vim.fn.jobstart({ 'zellij', 'action', 'new-pane', '--direction', 'left', '--cwd', cwd, '--close-on-exit', '--', shell }, { detach = true })
 end, { desc = 'Split Pane Left' })
 
 if vim.env.ZELLIJ then
