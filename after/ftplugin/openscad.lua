941609172b1ed2572e4be17ecc4b004097ab1e48
emtee <mtran0011@outlook.com>
2026-08-23 10:13:26 +1000

    Added openscad render keymap

--- after/ftplugin/openscad.lua
+++ after/ftplugin/openscad.lua
@@ -1,4 +1,6 @@
-vim.keymap.set('n', '<localleader>p', function()
+local export_formats = { '3mf', 'stl', 'amf', 'off', 'csg', 'dxf', 'svg', 'pdf', 'png' }
+
+local function run_openscad(format)
   if vim.fn.executable 'openscad' ~= 1 then
     vim.notify('OpenSCAD executable not found in PATH', vim.log.levels.ERROR)
     return
@@ -7,9 +9,33 @@ vim.keymap.set('n', '<localleader>p', function()
   vim.cmd.update()
 
   local path = vim.api.nvim_buf_get_name(0)
-  vim.system({ 'openscad', path }, { detach = true })
-end, {
+  local command = { 'openscad' }
+
+  if format then
+    command[#command + 1] = '-o'
+    command[#command + 1] = vim.fn.fnamemodify(path, ':r') .. '.' .. format
+  end
+
+  command[#command + 1] = path
+  vim.system(command, { detach = true })
+end
+
+vim.keymap.set('n', '<localleader>p', run_openscad, {
   buffer = true,
   desc = '[P]review in OpenSCAD',
+  silent = true,
+})
+
+vim.keymap.set('n', '<localleader>r', function()
+  vim.ui.select(export_formats, {
+    prompt = 'OpenSCAD export format:',
+  }, function(format)
+    if format then
+      run_openscad(format)
+    end
+  end)
+end, {
+  buffer = true,
+  desc = '[R]ender with OpenSCAD',
   silent = true,
 })
