ba606aadf67fae2d3d270186c6f6df66432a076e
deltoss <m.tran@leapdev.io>
2026-09-17 16:09:19 +1000

    Enabled vim._core.ui2 is it's a dependency of tiny-cmdline.nvim

--- lua/custom/plugins/tiny-cmdline-nvim.lua
+++ lua/custom/plugins/tiny-cmdline-nvim.lua
@@ -2,6 +2,7 @@ return {
   "rachartier/tiny-cmdline.nvim",
   init = function()
     vim.o.cmdheight = 0
+    require("vim._core.ui2").enable({})
   end,
   config = function()
     require("tiny-cmdline").setup({
