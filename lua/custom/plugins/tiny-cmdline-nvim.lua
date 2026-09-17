bae1ca81e614c4217adab54fae565c3a4f64255c
deltoss <m.tran@leapdev.io>
2026-09-17 15:47:23 +1000

    Fixed tiny-cmdline.nvim integration with blink.nvim

--- lua/custom/plugins/tiny-cmdline-nvim.lua
+++ lua/custom/plugins/tiny-cmdline-nvim.lua
@@ -3,4 +3,9 @@ return {
   init = function()
     vim.o.cmdheight = 0
   end,
+  config = function()
+    require("tiny-cmdline").setup({
+      on_reposition = require("tiny-cmdline").adapters.blink,
+    })
+  end,
 }
